c5f0783411efe4b2b219b8f2879179a97a37de9f
bliu <boyangliu@yahoo.com>
2021-03-18 22:47:30 -0400

    cleanup and bug fix

--- scheme_proj/BufferReader.cpp
+++ scheme_proj/BufferReader.cpp
@@ -14,23 +14,16 @@ void BufferReader::readBuffer() {
     buffer_ += line;
 }
 
+namespace {
+
+bool isDelimiter(char c) { //treat ` and @ and "
+    return (isspace(c) || (c == '(') || (c == ')'));
+}
+
 void dumpBuffer(std::string& buf, int pos) {
     std::cout << "current position:" << pos << '\n';
     std::cout << "processed:" << buf.substr(0, pos) << '\n';
     std::cout << "unprocessed:" << buf.substr(pos) << '\n';
-}
-
-int listEnd(std::string& buffer, int pos) { //bug when string has ( or )
-    assert(buffer[pos] == '(');
-    int idx = pos+1;
-    int count = 1;
-    while (idx < buffer.size()) {
-        if (buffer[idx] == '(') ++count;
-        else if (buffer[idx] == ')') --count;
-        if (count == 0) return ++idx;
-        ++idx;
-    }
-    return pos;
 }
 
 int stringEnd(std::string& buffer, int pos) {
@@ -43,18 +36,14 @@ int stringEnd(std::string& buffer, int pos) {
     }
     return pos;
 }
-namespace {
-bool isDelimiter(char c) {
-    return (isspace(c) || (c == '(') || (c == ')'));
-}
-}
+
 int atomEnd(std::string& buffer, int pos) {
     assert(!isspace(buffer[pos]));
     int idx = pos+1;
-    char c = buffer[idx];
-    if (isspace(c) || c == ')') return pos; //without this line subtle bug
-    while (idx < buffer.size()) { char c = buffer[idx]; if (isDelimiter(c)) break; ++idx; } //treat ` and "
+    while (idx < buffer.size()) { char c = buffer[idx]; if (isDelimiter(c)) break; ++idx; }
     return idx-1;
+}
+
 }
 
 Expression* BufferReader::readCdr() {
@@ -146,42 +135,9 @@ Expression* BufferReader::readNumber() {
     int atomEnd = ::atomEnd(buffer_, atomBeg);
     std::string token = buffer_.substr(atomBeg, atomEnd-atomBeg+1);
     readPos_ = atomEnd+1; //skipped validation of token as number
-    std::cout << "number token:" << token << " size: " << token.size() << " beg: " << atomBeg << " end: " << atomEnd << " readPos_:" << readPos_ << '\n';
+    //std::cout << "number token:" << token << " size: " << token.size() << " beg: " << atomBeg << " end: " << atomEnd << " readPos_:" << readPos_ << '\n';
     return new Expression(Atom((long)atoi(token.c_str())));
 }
-
-/*
-Expression* BufferReader::readCharacter() {
-    char c = buffer_[readPos_];
-    assert(c == '#');
-    int atomBeg = readPos_;
-    int atomEnd = ::atomEnd(buffer_, atomBeg);
-    std::string token = buffer_.substr(atomBeg, atomEnd);
-    if (token == "#\\space") {
-        readPos_ = atomEnd;
-        return new Expression(Atom(' '));
-    } else if (token == "#\\newline") {
-        readPos_ = atomEnd;
-        return new Expression(Atom('\n'));
-    }
-    if (token.size() != 3) {
-        std::cerr << "unexpexted character " << token << '\n';
-        return nullptr;
-    }
-    readPos_ = atomEnd;
-    return new Expression(Atom(token[2]));
-}
-
-Expression* BufferReader::readBoolean() {
-    char c = buffer_[readPos_];
-    assert(c == '#');
-    char n = buffer_[readPos_+1];
-    if (n == 't') { ++readPos_; return _true; }
-    else if (n == 'f') { ++readPos_; return _false; }
-    std::cerr << "unexpected #" << n << '\n';
-    return nullptr;
-}
-*/
 
 Expression* BufferReader::readHash() {
     char c = buffer_[readPos_];
@@ -190,14 +146,14 @@ Expression* BufferReader::readHash() {
     int atomEnd = ::atomEnd(buffer_, atomBeg);
     if (atomBeg != atomEnd) {
         std::string token = buffer_.substr(atomBeg, atomEnd-atomBeg+1);
-        if (token == "#t") { readPos_ = atomEnd; return _true; }
-        else if (token == "#f") { readPos_ = atomEnd; return _false; }
-        else if (token == "#\\space") { readPos_ = atomEnd; return new Expression(Atom(' ')); }
-        else if (token == "#\\newline") { readPos_ = atomEnd; return new Expression(Atom('\n')); }
-        else if (token.size() == 3 && token[1] == '\\') { readPos_ = atomEnd; return new Expression(Atom(token[2])); }
+        if (token == "#t") { readPos_ = atomEnd+1; return _true; }
+        else if (token == "#f") { readPos_ = atomEnd+1; return _false; }
+        else if (token == "#\\space") { readPos_ = atomEnd+1; return new Expression(Atom(' ')); }
+        else if (token == "#\\newline") { readPos_ = atomEnd+1; return new Expression(Atom('\n')); }
+        else if (token.size() == 3 && token[1] == '\\') { readPos_ = atomEnd+1; return new Expression(Atom(token[2])); }
         else {
             std::cerr << "discard unexpected " << token << '\n';
-            readPos_ = atomEnd;
+            readPos_ = atomEnd+1;
             return nullptr;
         }
     } else return nullptr;
@@ -208,12 +164,10 @@ Expression* BufferReader::readSymbol() {
     assert(!isspace(c));
     int atomBeg = readPos_;
     int atomEnd = ::atomEnd(buffer_, atomBeg);
-    //if (atomBeg != atomEnd) {
-        readPos_ = atomEnd+1;
-        std::string symbol = buffer_.substr(atomBeg, atomEnd-atomBeg+1);
-        std::cout << "symbol:" << symbol << ":\n";
-        return makeSymbol(buffer_.substr(atomBeg, atomEnd-atomBeg+1));
-    //} else return nullptr;
+    readPos_ = atomEnd+1;
+    std::string symbol = buffer_.substr(atomBeg, atomEnd-atomBeg+1);
+    //std::cout << "symbol:" << symbol << ":\n";
+    return makeSymbol(buffer_.substr(atomBeg, atomEnd-atomBeg+1));
 }
 
 //when fail should not change readPos_
