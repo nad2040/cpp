cc74a0415d9ad204b29efe1dd891023bb3424be5
bliu <boyangliu@yahoo.com>
2021-03-20 19:09:11 -0400

    make output proper header and src file

--- scheme_proj/build2.sh
+++ scheme_proj/build2.sh
@@ -1,1 +1,1 @@
-g++ -g -o scheme scheme2.cpp primitive_proc_2.cpp Sxpressions.cpp env.cpp eval.cpp ReadTokenizeParse.cpp
+g++ -g -o scheme scheme2.cpp primitive_proc_2.cpp Sxpressions.cpp env.cpp eval.cpp ReadTokenizeParse.cpp output.cpp
