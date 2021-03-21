8219d11538809523baba5fa9cee58a87e34e0184
bliu <boyangliu@yahoo.com>
2021-03-21 11:18:10 -0400

    wrapped bool

--- scheme_proj/example/atom.cpp
+++ scheme_proj/example/atom.cpp
@@ -3,16 +3,19 @@
 #include <variant>
 #include <vector>
 
+struct mybool { bool b; };
+
 struct Atom {
+
     enum { BOOL, CHAR, INT, STR, SYM } atomType_;
-    //std::variant<bool, char, int, std::string> value_; //with bool and int together it cause crash
-    std::variant<char, int, std::string> value_;
+    std::variant<mybool, char, int, std::string> value_; //with bool and int together it cause crash
+    //std::variant<char, int, std::string> value_;
 };
 
 void display(const Atom& a) {
     switch(a.atomType_) {
-        case Atom::BOOL: std::cout << "BOOL:" << std::get<int>(a.value_) << '\n'; return;
-        //case Atom::BOOL: std::cout << "BOOL:" << std::get<bool>(a.value_) << '\n'; return;
+        //case Atom::BOOL: std::cout << "BOOL:" << std::get<int>(a.value_) << '\n'; return;
+        case Atom::BOOL: std::cout << "BOOL:" << std::get<mybool>(a.value_).b << '\n'; return;
         case Atom::CHAR: std::cout << "CHAR:" << std::get<char>(a.value_) << '\n'; return;
         case Atom::INT: std::cout << "INT:" << std::get<int>(a.value_) << '\n'; return;
         case Atom::STR: std::cout << "STR:" << std::get<std::string>(a.value_) << '\n'; return;
@@ -23,7 +26,7 @@ void display(const Atom& a) {
 
 int main() {
     std::vector<Atom> atoms;
-    atoms.push_back({Atom::BOOL, true});
+    atoms.push_back({Atom::BOOL, (mybool){true}});
     atoms.push_back({Atom::CHAR, 'c'});
     atoms.push_back({Atom::INT, 123});
     atoms.push_back({Atom::STR, "hello"});
