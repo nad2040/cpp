8edc115b7a6215ca47810d6393ab94637c1bcc6f
bliu <boyangliu@yahoo.com>
2021-03-21 07:58:09 -0400

    more file moves

--- scheme_proj/output.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include "Sxpressions.h"
-void write(std::ostream& os, Expression *expr);
-void writeList(std::ostream& os, Expression *list); 
-std::ostream& operator<<(std::ostream& os, Expression* expr);
-std::ostream& operator<<(std::ostream& os, const Expression& expr);
--- scheme_proj/scheme2/build2.sh
+++ scheme_proj/scheme2/build2.sh
@@ -1,1 +1,1 @@
-g++ -std=c++17 -g -o scheme scheme2.cpp primitive_proc_2.cpp Sxpressions.cpp env.cpp eval.cpp ReadTokenizeParse.cpp output.cpp
+g++ -std=c++17 -g -o scheme scheme2.cpp primitive_proc.cpp Sxpressions.cpp env.cpp eval.cpp ReadTokenizeParse.cpp output.cpp
