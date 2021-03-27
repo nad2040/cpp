457ba5331ddca0a859b415083d4fa3213f44d038
bliu <boyangliu@yahoo.com>
2021-03-27 07:27:45 -0400

    move internal to private section

--- scheme_proj/scheme2/env.h
+++ scheme_proj/scheme2/env.h
@@ -9,27 +9,28 @@ public:
         empty_env = empty_list;
         global_env = makeEnv();
     }
-    Expression* setupEnv(); 
-    void populateEnv(Expression* env);
-    Expression* makeEnv();
 
+    //required by primitive
+    Expression* setupEnv(); //nullEnvProc
+    Expression* makeEnv(); //envProc
+
+    //required by eval
     Expression* extendEnv(Expression *vars, Expression *vals, Expression *base_env); 
+    Expression* lookupVarValue(Expression *var, Expression *env); 
+    void defVar(Expression *var, Expression *val, Expression *env); 
+    void setVarValue(Expression *var, Expression *val, Expression *env); 
+
+    Expression* empty_env;
+    Expression* global_env;
+
+private:
+    void populateEnv(Expression* env);
+
     Expression* makeFrame(Expression *variables, Expression *values);
     Expression* frameVar(Expression *frame); 
     Expression* frameValues(Expression *frame); 
     void addBindingToFrame(Expression *var, Expression *val, Expression *frame); 
     Expression* enclosingEnvironment(Expression *env);
     Expression* firstFrame(Expression *env); 
-
-    Expression* lookupVarValue(Expression *var, Expression *env); 
-    void setVarValue(Expression *var, Expression *val, Expression *env); 
-    void defVar(Expression *var, Expression *val, Expression *env); 
-
-    Expression* empty_env;
-    Expression* global_env;
-/*
-private:
-    Expression* env_;
-    */
 };
 
